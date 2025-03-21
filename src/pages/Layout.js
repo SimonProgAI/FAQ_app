12dd0961238ea4a60a04070dfb803810971d050f
Simon Lupien <lupiensimon@hotmail.com>
2025-03-21 09:32:09 -0500

    BrowserRouter working in App.js

---
+++ src/pages/Layout.js
@@ -0,0 +1,11 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+    return(
+        <div>
+            <h1>Something</h1>
+        </div>
+    )
+}
+
+export default Layout;
