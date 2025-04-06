2af29f730e376e1d1826984756a3db24235f6b0d
Simon Lupien <lupiensimon@hotmail.com>
2025-04-06 18:35:23 -0500

    minor edits

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
+import '../App.css';
 import React from "react";
 import { useState, useRef, useEffect} from 'react';
 import { Link, useNavigate } from "react-router-dom";
-import '../App.css';
 
 const Login = ({uname, unameSetter}) => {
     const unameRef = useRef();
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,7 @@
+import '../App.css';
 import React from "react";
 import {useState, useRef} from "react";
 import {useNavigate} from 'react-router-dom';
-import '../App.css';
-
 
 function Register(){
     const navigate = useNavigate();
